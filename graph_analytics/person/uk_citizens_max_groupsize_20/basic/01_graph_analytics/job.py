d182bffb8bfc2e96314a83e2a28ca0179277f311
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2021-07-21 22:47:05 +0100

    splink graph works

--- graph_analytics/person/uk_citizens_max_groupsize_20/basic/01_graph_analytics/job.py
+++ graph_analytics/person/uk_citizens_max_groupsize_20/basic/01_graph_analytics/job.py
@@ -13,6 +13,8 @@ from constants import get_paths_from_job_path
 from custom_logger import get_custom_logger
 
 from splink_graph.cluster_metrics import cluster_basic_stats, cluster_main_stats
+from splink_graph.node_metrics import eigencentrality
+from splink_graph.edge_metrics import edgebetweeness
 
 sc = SparkContext()
 glue_context = GlueContext(sc)
@@ -35,7 +37,7 @@ args = getResolvedOptions(
 
 trial_run = args["trial_run"] == "true"
 if trial_run:
-    PARALLELISM = 20
+    PARALLELISM = 40
 else:
     PARALLELISM = 100
 
@@ -45,13 +47,6 @@ else:
 custom_log = get_custom_logger(args["JOB_RUN_ID"])
 
 custom_log.info(f"Snapshot date is {args['snapshot_date']}")
-
-
-def getShowString(df, n=20, truncate=True, vertical=False):
-    if isinstance(truncate, bool) and truncate:
-        return df._jdf.showString(n, 20, vertical)
-    else:
-        return df._jdf.showString(n, int(truncate), vertical)
 
 
 # Output paths can be derived from the path
@@ -68,51 +63,58 @@ df_edges.createOrReplaceTempView("df_edges")
 df_clusters = spark.read.parquet(paths["clusters_path"])
 df_clusters.createOrReplaceTempView("df_clusters")
 
+custom_log.info(f"{df_edges.count()} =df_edges.count()")
+custom_log.info(f"{df_clusters.count()} =df_clusters.count()")
+
+# where  df_c_1.cluster_medium = 13
 sql = """
 select
     unique_id_l as src,
     unique_id_r as dst,
     tf_adjusted_match_prob as weight,
-    cast(df_c_1.cluster_medium as int) as cluster_id
-from df_edges
+    df_c_1.cluster_medium as cluster_id
+from
+    df_edges
 
 left join
-df_clusters df_c_1 on
-df_edges.unique_id_l = df_c_1.unique_id
+    df_clusters df_c_1
+    on df_edges.unique_id_l = df_c_1.unique_id
 
 left join
-df_clusters as df_c_2 on
-df_edges.unique_id_r = df_c_2.unique_id
+    df_clusters as df_c_2
+    on df_edges.unique_id_r = df_c_2.unique_id
 
-where df_c_1.cluster_medium = 13
-
-and df_c_1.cluster_medium = df_c_2.cluster_medium
+where
+    df_c_1.cluster_medium = df_c_2.cluster_medium
 """
+
 df = spark.sql(sql)
 
-custom_log.info(getShowString(df))
-schema = df.schema.jsonValue()
-custom_log.info(json.dumps(schema, indent=4))
+custom_log.info(f"{df.count()} =df.count()")
+
 
 cluster_basic_stats_df = cluster_basic_stats(df)
 
-out_path_root = paths["graph_analytics_path"]
-
-out_path = os.path.join(out_path_root, "basic_cluster_metrics")
-cluster_basic_stats_df = cluster_basic_stats_df.repartition(1)
-cluster_basic_stats_df.write.mode("overwrite").parquet(out_path)
-
-
 cluster_main_stats_df = cluster_main_stats(df)
 
-custom_log.info(getShowString(cluster_main_stats_df))
-# cluster_all_stats_df = cluster_basic_stats_df.join(
-#     cluster_main_stats_df, on=["cluster_id"], how="left"
-# )
+cluster_all_stats_df = cluster_basic_stats_df.join(
+    cluster_main_stats_df, on=["cluster_id"], how="left"
+)
 
 
-# out_path = os.path.join(out_path_root, "main_cluster_metrics")
-# cluster_main_stats_df = cluster_main_stats_df.repartition(1)
-# cluster_main_stats_df.write.mode("overwrite").parquet(out_path)
+out_path_root = paths["graph_analytics_path"]
 
-# alueError: Vertex ID column id missing from vertex DataFrame, which has columns: unique_id,source_dataset,commit_hash
+out_path = os.path.join(out_path_root, "all_cluster_metrics")
+cluster_all_stats_df = cluster_all_stats_df.repartition(1)
+cluster_all_stats_df.write.mode("overwrite").parquet(out_path)
+
+node_df = eigencentrality(df, distance_colname="weight")
+out_path = os.path.join(out_path_root, "node_metrics")
+node_df = node_df.repartition(1)
+node_df.write.mode("overwrite").parquet(out_path)
+
+
+edge_metrics_df = edgebetweeness(df, distance_col="weight")
+edge_metrics_df = edge_metrics_df.repartition(10)
+out_path = os.path.join(out_path_root, "edge_metrics")
+edge_metrics_df.write.mode("overwrite").parquet(out_path)
