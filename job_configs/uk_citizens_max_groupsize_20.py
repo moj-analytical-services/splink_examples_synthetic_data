242135aafb8635675883cdeb8199a8ababd0d829
Mamonu <thorosm@msn.com>
2021-09-07 12:58:39 +0000

    model4 and jobs running in right order

--- job_configs/uk_citizens_max_groupsize_20.py
+++ job_configs/uk_citizens_max_groupsize_20.py
@@ -76,11 +76,12 @@ uk_citizens_max_groupsize_20 = {
         "allocated_capacity": 5,
         "version": "v01",
         "list_python_modules": [
-            "splink-graph==0.4.15",
+            "networkx==2.5.1",
+            "gensim==3.8.3",
+            "splink-graph==0.4.19",
             "pyarrow==0.15.1",
             "numpy==1.19.5",
             "graphframes==0.6.0",
-            "networkx==2.5.1"
         ],
         "additional_job_args": {
             "--conf": "spark.executorEnv.ARROW_PRE_0_15_IPC_FORMAT=1"
--- pipelines/uk_citizens_max_groupsize_20/person/basic/pipeline.sh
+++ pipelines/uk_citizens_max_groupsize_20/person/basic/pipeline.sh
@@ -10,9 +10,10 @@ set -o pipefail
 python jobrunner.py --job_path model_training/person/uk_citizens_max_groupsize_20/basic/03_combine_blocks
 python jobrunner.py --job_path edge_generation/person/uk_citizens_max_groupsize_20/basic
 python jobrunner.py --job_path cluster_generation/person/uk_citizens_max_groupsize_20/basic/01_cluster
+python jobrunner.py --job_path graph_analytics/person/uk_citizens_max_groupsize_20/basic/01_graph_analytics
 python jobrunner.py --job_path qa/compute_accuracy/person/uk_citizens_max_groupsize_20/basic
 python jobrunner.py --job_path qa/cluster_truth/person/uk_citizens_max_groupsize_20/basic
-python jobrunner.py --job_path graph_analytics/person/uk_citizens_max_groupsize_20/basic/01_graph_analytics
+
 
 # python jobrunner.py --job_path node_generation/standardised_nodes/uk_citizens_max_groupsize_20/basic/person/01_generate_standardised_nodes
 # python jobrunner.py --job_path node_generation/standardised_nodes/uk_citizens_max_groupsize_20/basic/person/02_profile_standardised_nodes
