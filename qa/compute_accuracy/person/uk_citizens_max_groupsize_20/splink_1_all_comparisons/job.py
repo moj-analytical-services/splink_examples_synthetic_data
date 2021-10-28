b5c53d1b9d1be7361625185203047c3ec1836348
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2021-10-28 15:24:06 +0100

    comparison complete

--- qa/compute_accuracy/person/uk_citizens_max_groupsize_20/splink_1_all_comparisons/job.py
+++ qa/compute_accuracy/person/uk_citizens_max_groupsize_20/splink_1_all_comparisons/job.py
@@ -140,23 +140,137 @@ df_e_with_labels = spark.read.parquet(output_path)
 
 # Thin out the ROC curve by limiting the number of unique probabilities
 
-# score_colname = _get_score_colname(df_e_with_labels)
+score_colname = _get_score_colname(df_e_with_labels)
 
-# log2_bf_expr = f"log2({score_colname}/(1-{score_colname}))"
+log2_bf_expr = f"log2({score_colname}/(1-{score_colname}))"
 
-# expr = f"""
-# case
-#     when {score_colname} = 0.0 then -100
-#     when {log2_bf_expr} < -20 or {log2_bf_expr} > 20 then round({log2_bf_expr},0)
-#     else round({log2_bf_expr},1)
-# end
+expr = f"""
+case
+    when {score_colname} = 0.0 then -100
+    when {log2_bf_expr} < -30 or {log2_bf_expr} > 30 then {log2_bf_expr}
+    else round({log2_bf_expr},1)
+end
 
-# """
-# df = df.withColumn("rounded", f.expr("round(d/5, 1)*5"))
+"""
+
+df_e_with_labels = df_e_with_labels.withColumn("bf_temp", f.expr(expr))
 
 df_e_with_labels = df_e_with_labels.withColumn(
-    "bf_temp", f.expr("round(df_e__tf_adjusted_match_weight/2, 1)*2")
+    "bf_temp", f.expr("round(bf_temp/2, 1)*2")
 )
+
+df_e_with_labels = df_e_with_labels.withColumn(
+    "tf_adjusted_match_prob", f.expr("power(2, bf_temp)/(1+power(2,bf_temp))")
+)
+df_e_with_labels = df_e_with_labels.drop("bf_temp")
+
+charts_path_roc = output_paths["charts_roc_path"]
+
+chart = roc_chart(df_e_with_labels, spark)
+chart.save("roc_chart.html")
+write_local_file_to_s3(
+    "roc_chart.html",
+    charts_path_roc,
+    overwrite=True,
+)
+
+# Make the ROC chart comparative
+
+edges_path = output_paths["edges_path"]
+df_edges = spark.read.parquet(edges_path)
+
+
+# Create labelled data from person id
+SOURCE_NODES_PATH = output_paths["source_nodes_path"]
+SOURCE_NODES_PATH = SOURCE_NODES_PATH.replace("splink_1_all_comparisons", "basic")
+
+df_source = spark.read.parquet(SOURCE_NODES_PATH)
+
+
+df_source.createOrReplaceTempView("df_source")
+df_edges.createOrReplaceTempView("df_edges")
+
+sql = """
+select
+    df_l.unique_id as unique_id_l,
+    df_l.source_dataset as source_dataset_l,
+    df_r.unique_id as unique_id_r,
+    df_r.source_dataset as source_dataset_r,
+    1.0 as clerical_match_score
+
+from df_source as df_l
+left join df_source as df_r
+
+on df_l.cluster = df_r.cluster
+
+where df_l.unique_id < df_r.unique_id
+
+union all
+
+select
+    df_edges.unique_id_l as unique_id_l,
+    df_edges.source_dataset_l as source_dataset_l,
+
+    df_edges.unique_id_r as unique_id_r,
+    df_edges.source_dataset_r as source_dataset_r,
+    0.0 as clerical_match_score
+    from df_edges
+    where  cluster_l != cluster_r
+"""
+df_labels = spark.sql(sql)
+
+
+df_e_with_labels = labels_with_splink_scores(
+    df_labels,
+    df_edges,
+    "unique_id",
+    spark,
+    source_dataset_colname="source_dataset",
+    retain_all_cols=True,
+)
+
+
+df_e_with_labels = df_e_with_labels.repartition(10)
+df_e_with_labels = df_e_with_labels.withColumn(
+    "commit_hash", f.lit(args["commit_hash"])
+)
+
+
+output_path = output_paths["labels_with_scores_path"]
+
+
+df_e_with_labels.write.mode("overwrite").parquet(output_path)
+
+custom_log.info(f"outputting scores with labels to to {output_path}")
+
+PARALLELISM = 20
+spark.conf.set("spark.sql.shuffle.partitions", PARALLELISM)
+spark.conf.set("spark.default.parallelism", PARALLELISM)
+
+
+df_e_with_labels = spark.read.parquet(output_path)
+
+# Thin out the ROC curve by limiting the number of unique probabilities
+
+score_colname = _get_score_colname(df_e_with_labels)
+
+log2_bf_expr = f"log2({score_colname}/(1-{score_colname}))"
+
+expr = f"""
+case
+    when {score_colname} = 0.0 then -100
+    when {log2_bf_expr} < -30 or {log2_bf_expr} > 30 then {log2_bf_expr}
+    else round({log2_bf_expr},1)
+end
+
+"""
+
+df_e_with_labels = df_e_with_labels.withColumn("bf_temp", f.expr(expr))
+
+df_e_with_labels = df_e_with_labels.withColumn(
+    "bf_temp", f.expr("round(bf_temp/2, 1)*2")
+)
+
 df_e_with_labels = df_e_with_labels.withColumn(
     "tf_adjusted_match_prob", f.expr("power(2, bf_temp)/(1+power(2,bf_temp))")
 )
